60c2b7b633e2f2b9375922f85b7fd39aade0d2a5
Milena Tomova <kokolina1888@gmail.com>
2020-07-14 13:45:44 +0300

    shopping list
    shopping list component styling finished

--- sr-list/src/components/shoppingList/index.js
+++ sr-list/src/components/shoppingList/index.js
@@ -8,83 +8,81 @@ class ShoppingList extends Component {
           <div>
             <Breadcrumb>Shopping List</Breadcrumb>
             <div className="receipe-content-area">
-                <div className="container">
+              <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-4">
-                       
-                        <div className="single-preparation-step d-flex">
-                            <h4>01.</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. </p>
-                            
-                        </div>
-                        <div className="single-preparation-step d-flex">
-                            <h4>01.</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. </p>
-                            
-                        </div>
-                         <div className="single-preparation-step d-flex">
-                            <h4>01.</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. </p>
-                            
-                        </div>                       
-                    </div>                   
-                    <div className="col-12 col-lg-8">
-                        <div className="ingredients">
-                            <h4>Shopping List</h4>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                                <label className="custom-control-label" for="customCheck1">4 Tbsp (57 gr) butter</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck2"/>
-                                <label className="custom-control-label" for="customCheck2">2 large eggs</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck3"/>
-                                <label className="custom-control-label" for="customCheck3">2 yogurt containers granulated sugar</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck4"/>
-                                <label className="custom-control-label" for="customCheck4">1 vanilla or plain yogurt, 170g container</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck5"/>
-                                <label className="custom-control-label" for="customCheck5">2 yogurt containers unbleached white flour</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck6"/>
-                                <label className="custom-control-label" for="customCheck6">1.5 yogurt containers milk</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck7"/>
-                                <label className="custom-control-label" for="customCheck7">1/4 tsp cinnamon</label>
-                            </div>
-
-                            
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck8"/>
-                                <label className="custom-control-label" for="customCheck8">1 cup fresh blueberries </label>
-                            </div>
-                        </div>
+                  <div className="col-12 col-lg-4">
+                    <div className="single-preparation-step d-flex">
+                      <span className={styles.recipe}>01.</span>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vestibulum nec varius dui.
+                      </p>
                     </div>
+                    <div className="single-preparation-step d-flex">
+                      <span className={styles.recipe}>01.</span>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vestibulum nec varius dui.
+                      </p>
+                    </div>
+                    <div className="single-preparation-step d-flex">
+                      <span className={styles.recipe}>01.</span>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vestibulum nec varius dui.{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <div className={styles['container'] + " col-12 col-lg-8"}>
+                      <h4 className={styles.title}>Shopping List</h4>
+                      <table className="table table-striped">
+                        <thead>
+                          <tr>
+                            <td>#</td>
+                            <td>Product</td>
+                            <td>Quantity</td>
+                            <td>Units</td>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>1</td>
+                            <td>Butter</td>
+                            <td>57</td>
+                            <td>g</td>
+                          </tr>
+                          <tr>
+                            <td>2</td>
+                            <td>Eggs</td>
+                            <td>2</td>
+                            <td>-</td>
+                          </tr>
+                          <tr>
+                            <td>3</td>
+                            <td>sugar</td>
+                            <td>300</td>
+                            <td>g</td>
+                          </tr>
+                          <tr>
+                            <td>4</td>
+                            <td>youghurt</td>
+                            <td>300</td>
+                            <td>g</td>
+                          </tr>
+                          <tr>
+                            <td>5</td>
+                            <td>flour</td>
+                            <td>500</td>
+                            <td>g</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-          </div>
         );
     }
 }
