4b8918e7c2cd5fa54b15a09bddbc3792ec712980
Milena Tomova <kokolina1888@gmail.com>
2020-07-14 13:52:58 +0300

    shopping list
    link to see the recipe added

--- sr-list/src/components/shoppingList/index.js
+++ sr-list/src/components/shoppingList/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from "./index.module.css";
 import Breadcrumb from '../breadcrumb'
+import Link from '../UI/link/link';
 
 class ShoppingList extends Component {
     render() {
@@ -16,6 +17,9 @@ class ShoppingList extends Component {
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Vestibulum nec varius dui.
+                        <Link href="/" type="shopping-list">
+                          See recipe...
+                        </Link>
                       </p>
                     </div>
                     <div className="single-preparation-step d-flex">
@@ -23,6 +27,9 @@ class ShoppingList extends Component {
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Vestibulum nec varius dui.
+                        <Link href="/" type="shopping-list">
+                          See recipe...
+                        </Link>
                       </p>
                     </div>
                     <div className="single-preparation-step d-flex">
@@ -30,59 +37,61 @@ class ShoppingList extends Component {
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Vestibulum nec varius dui.{" "}
+                        <Link href="/" type="shopping-list">
+                          See recipe...
+                        </Link>
                       </p>
                     </div>
                   </div>
-                  <div className={styles['container'] + " col-12 col-lg-8"}>
-                      <h4 className={styles.title}>Shopping List</h4>
-                      <table className="table table-striped">
-                        <thead>
-                          <tr>
-                            <td>#</td>
-                            <td>Product</td>
-                            <td>Quantity</td>
-                            <td>Units</td>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>1</td>
-                            <td>Butter</td>
-                            <td>57</td>
-                            <td>g</td>
-                          </tr>
-                          <tr>
-                            <td>2</td>
-                            <td>Eggs</td>
-                            <td>2</td>
-                            <td>-</td>
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td>sugar</td>
-                            <td>300</td>
-                            <td>g</td>
-                          </tr>
-                          <tr>
-                            <td>4</td>
-                            <td>youghurt</td>
-                            <td>300</td>
-                            <td>g</td>
-                          </tr>
-                          <tr>
-                            <td>5</td>
-                            <td>flour</td>
-                            <td>500</td>
-                            <td>g</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      
-                    </div>
+                  <div className={styles["container"] + " col-12 col-lg-8"}>
+                    <h4 className={styles.title}>Shopping List</h4>
+                    <table className="table table-striped">
+                      <thead>
+                        <tr>
+                          <td>#</td>
+                          <td>Product</td>
+                          <td>Quantity</td>
+                          <td>Units</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Butter</td>
+                          <td>57</td>
+                          <td>g</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Eggs</td>
+                          <td>2</td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>sugar</td>
+                          <td>300</td>
+                          <td>g</td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td>youghurt</td>
+                          <td>300</td>
+                          <td>g</td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td>flour</td>
+                          <td>500</td>
+                          <td>g</td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
         );
     }
 }
