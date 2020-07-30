5ab174965e1e220a65a4137c0fbb201609a6ee56
Milena Tomova <kokolina1888@gmail.com>
2020-07-30 21:01:11 +0300

    statistics are set dynamically
    only total recipes statistics are get dynamically fro db, the rest are set using react hooks

--- sr-list/src/components/infos/statistics/index.js
+++ sr-list/src/components/infos/statistics/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StatisticsCard from '../statisticsCard';
+import {firebaseLooper} from '../../../shared'
 import styles from "./index.module.css";
+import { firebaseRecipes } from '../../../firebase';
 
 const Statistics = () => {
+  const [data, setTotal ] = useState({
+    total: 0,
+    salads: 0
+  })
+  useEffect(() => {
+    
+    firebaseRecipes      
+      .once("value")
+      .then((snapshot) => {
+        let fetchedRecipes = firebaseLooper(snapshot);
+        setTotal({
+        total: fetchedRecipes.length,
+        salads: 10
+      }
+        );
+      })
+      .catch((err) => {
+      });
+  }, [data])
+
+
+ 
     return (
       <div className={styles.container + " row"}>
         <div className="col-12 col-sm-6 col-lg-3">
-          <StatisticsCard type="total" title="Total" count="2326"/>
+          <StatisticsCard type="total" title="Total" count={data.total}/>
         </div>
         <div className="col-12 col-sm-6 col-lg-3">
-          <StatisticsCard type="snacks" title="Snacks" count="521"/>
+          <StatisticsCard type="snacks" title="Salads" count={data.salads}/>
         </div>
         <div className="col-12 col-sm-6 col-lg-3">
           <StatisticsCard type="rib" title="Meal" count="725"/>
